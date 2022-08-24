82fc3401edb428be9ca1722fae30f4ad23306852
Your Name <you@example.com>
2022-08-24 11:11:20 +0545

    Added templates and first view

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home' )
-               
-               ]
+    path('', views.home, name='home' ),
+    path('blog', views.blog, name='blog'),
+    path('blogpost/<str:slug>', views.blogpost, name='blogpost'),
+    path('contact', views.contact, name='contact'), 
+    path('search', views.search, name='search'),          
+]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'base.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+    
+def blogpost(request, slug):
+    slug = slug
+    print(slug)
+    context = {
+        'slug' : slug
+    }
+    return render(request, 'blogpost.html', context)
+
+def contact(request):
+   return render(request, 'contact.html')
+
+def search(request):
+    return render(request, 'search.html')
